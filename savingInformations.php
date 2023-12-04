c54684f3d5dc627217fdb29ed42910bdf50eb944
Hamza Akif Aydoğdu <h.a.akif@icloud.com>
2023-12-04 03:03:14 +0100

    finito

--- savingInformations.php
+++ savingInformations.php
@@ -9,20 +9,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = isset($_POST['email']) ? $_POST['email'] : '';
     $telephone = isset($_POST['telephone']) ? $_POST['telephone'] : '';
     $sexe = isset($_POST['sexe']) ? $_POST['sexe'] : '';
+    $fortnite = isset($_POST['fortnite']) ? $_POST['fortnite'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
 
-    // Créer un tableau associatif avec les données
-    $informations = array(
+    // Charger les données existantes s'il y en a
+    $informations = file_exists('informations.json') ? json_decode(file_get_contents('informations.json'), true) : array();
+
+    // Ajouter les nouvelles informations
+    $nouvellesInformations = array(
         'nom' => $nom,
         'prenom' => $prenom,
         'age' => $age,
         'lieu' => $lieu,
         'email' => $email,
         'telephone' => $telephone,
-        'sexe' => $sexe
+        'sexe' => $sexe,
+        'fortnite' => $fortnite,
+        'password' => $password
     );
 
+    // Ajouter les nouvelles informations au tableau existant
+    $informations[] = $nouvellesInformations;
+
     // Convertir le tableau en chaîne JSON
-    $informationsJSON = json_encode($informations);
+    $informationsJSON = json_encode($informations, JSON_PRETTY_PRINT);
 
     // Écrire la chaîne JSON dans un fichier
     file_put_contents('informations.json', $informationsJSON);
